b60cc22ea18ff375f20a8046c3039c76be4fd76b
gitMartinos <stt7693@gmail.com>
2022-06-15 18:32:38 +0300

    complite

--- SkillBox/Home Work 3/07_wall.py
+++ SkillBox/Home Work 3/07_wall.py
@@ -1,11 +1,23 @@
 # -*- coding: utf-8 -*-
 
 # (цикл for)
-import simple_draw
+import simple_draw as sd
 
 # Нарисовать стену из кирпичей. Размер кирпича - 100х50
 # Использовать вложенные циклы for
 
-# TODO здесь ваш код
+sd.resolution = (601, 601)
 
-simple_draw.pause()
+for y in range(0, 600, 100):
+    x = 0
+    for x in range(x, 600, 100):
+        start_poin = sd.get_point(x, y)
+        end_point = sd.get_point(x + 100, y + 50)
+        sd.rectangle(start_poin, end_point, width=1)
+for y in range(50, 600, 100):
+    x = -50
+    for x in range(x, 600, 100):
+        start_poin = sd.get_point(x, y)
+        end_point = sd.get_point(x + 100, y + 50)
+        sd.rectangle(start_poin, end_point, width=1)
+sd.pause()
--- SkillBox/Home Work 3/08_smile.py
+++ SkillBox/Home Work 3/08_smile.py
@@ -1,13 +1,32 @@
 # -*- coding: utf-8 -*-
 
 # (определение функций)
-import simple_draw
+import simple_draw as sd
 
 # Написать функцию отрисовки смайлика в произвольной точке экрана
 # Форма рожицы-смайлика на ваше усмотрение
 # Параметры функции: кордината X, координата Y, цвет.
 # Вывести 10 смайликов в произвольных точках экрана.
 
-# TODO здесь ваш код
+sd.resolution = (700, 700)
 
-simple_draw.pause()
+def smile(x, y, color):
+    start_point = sd.get_point(x, y)
+    start_point_2 = sd.get_point(x-15, y+20)
+    start_point_3 = sd.get_point(x+15, y+20)
+    sd.circle(start_point, 50, color, width=4)
+    sd.line(sd.get_point(x - 25, y - 5), sd.get_point(x - 10, y - 20), color, 2)
+    sd.line(sd.get_point(x - 10, y - 20), sd.get_point(x + 10, y - 20), color, 2)
+    sd.line(sd.get_point(x + 10, y - 20), sd.get_point(x + 24, y - 5), color, 2)
+    sd.circle(start_point_2, 5, color, width=1)
+    sd.circle(start_point_3, 5, color, width=1)
+
+
+for i in range(10):
+    point = sd.random_point()
+    x = point.x
+    y = point.y
+    color = sd.random_color()
+    smile(x, y, color)
+
+sd.pause()
