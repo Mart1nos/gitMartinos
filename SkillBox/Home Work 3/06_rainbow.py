0e5e83093f2675b8aee8895723ff8382d8480f13
gitMartinos <stt7693@gmail.com>
2022-06-15 12:30:22 +0300

    finish the second

--- SkillBox/Home Work 3/06_rainbow.py
+++ SkillBox/Home Work 3/06_rainbow.py
@@ -7,12 +7,26 @@ import simple_draw as sd
 rainbow_colors = (sd.COLOR_RED, sd.COLOR_ORANGE, sd.COLOR_YELLOW, sd.COLOR_GREEN,
                   sd.COLOR_CYAN, sd.COLOR_BLUE, sd.COLOR_PURPLE)
 
+sd.resolution = (500, 500)
+
 # Нарисовать радугу: 7 линий разного цвета толщиной 4 с шагом 5 из точки (50, 50) в точку (350, 450)
-# TODO здесь ваш код
+start_x = 50
+start_y = 50
+end_x = 350
+end_y = 450
+for color in rainbow_colors:
+    start_point = sd.get_point(start_x, start_y)
+    end_point = sd.get_point(end_x, end_y)
+    sd.line(start_point=start_point, end_point=end_point, color=color, width=4)
+    start_x += 5
+    end_x +=5
+
+
 
 # Усложненное задание, делать по желанию.
 # Нарисовать радугу дугами от окружности (cсм sd.circle) за нижним краем экрана,
 # поэкспериментировать с параметрами, что бы было красиво
-# TODO здесь ваш код
+
+
 
 sd.pause()
