a240b8b42164ff3e9ef3c44b9cc9c3af5a6da794
gitMartinos <stt7693@gmail.com>
2022-06-15 15:48:06 +0300

    complite

--- SkillBox/Home Work 3/06_rainbow.py
+++ SkillBox/Home Work 3/06_rainbow.py
@@ -10,6 +10,7 @@ rainbow_colors = (sd.COLOR_RED, sd.COLOR_ORANGE, sd.COLOR_YELLOW, sd.COLOR_GREEN
 sd.resolution = (500, 500)
 
 # Нарисовать радугу: 7 линий разного цвета толщиной 4 с шагом 5 из точки (50, 50) в точку (350, 450)
+
 start_x = 50
 start_y = 50
 end_x = 350
@@ -22,11 +23,16 @@ for color in rainbow_colors:
     end_x +=5
 
 
-
 # Усложненное задание, делать по желанию.
 # Нарисовать радугу дугами от окружности (cсм sd.circle) за нижним краем экрана,
 # поэкспериментировать с параметрами, что бы было красиво
 
+point = sd.get_point(350, -50)
+radius = 400
+
+for colors in rainbow_colors:
+    radius += 15
+    sd.circle(center_position=point, radius=radius, width=15, color=colors)
 
 
 sd.pause()
