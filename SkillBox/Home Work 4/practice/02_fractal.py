e5f060e9cf711dda7c82e465d5458d37ace22299
gitMartinos <stt7693@gmail.com>
2022-06-18 12:17:18 +0300

    work 4 lessen

--- SkillBox/Home Work 4/practice/02_fractal.py
+++ SkillBox/Home Work 4/practice/02_fractal.py
@@ -6,38 +6,41 @@ sd.resolution = (600, 800)
 
 # нарисовать ветку дерева из точки (300, 5) вертикально вверх длиной 100
 
-point_0 = sd.get_point(300, 5)
-
+point_0 = sd.get_point(300, 5) # Начальная точка по x и y
+v1 = sd.get_vector(start_point=point_0, angle=90, length=200, width=3)
+v1.draw()
 
 # сделать функцию рисования ветки из заданной точки,
 # заданной длины, с заданным наклоном
-# def branch(point, angle, length):
-#     v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=3)
-#     v1.draw()
-#     return v1.end_point
 
-# angle_0 = 90
-# length_0 = 200
-# next_point = branch(point=point_0, angle=angle_0, length=length_0)
-# next_angle = angle_0 - 30
-# next_length = length_0 * .75
-# next_point = branch(point=next_point, angle=next_angle, length=next_length)
-# next_angle = next_angle - 30
-# next_length = next_length * .75
-# next_point = branch(point=next_point, angle=next_angle, length=next_length)
+def branch(point, angle, length):
+    v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=3)
+    v1.draw()
+    return v1.end_point
+
+angle_0 = 90
+length_0 = 200
+next_point = branch(point=point_0, angle=angle_0, length=length_0)
+next_angle = angle_0 - 30
+next_length = length_0 * .75
+next_point = branch(point=next_point, angle=next_angle, length=next_length)
+next_angle = next_angle - 30
+next_length = next_length * .75
+next_point = branch(point=next_point, angle=next_angle, length=next_length)
 
 # написать цикл рисования ветвей с постоянным уменьшением длины на 25% и отклонением на 30 градусов
-# angle_0 = 90
-# length_0 = 200
-#
-# next_angle = angle_0
-# next_length = length_0
-# next_point = point_0
-#
-# while next_length > 1:
-#     next_point = branch(point=next_point, angle=next_angle, length=next_length)
-#     next_angle = next_angle - 30
-#     next_length = next_length * .75
+
+angle_0 = 90 # Угол наклона
+length_0 = 200  # Длинна
+
+next_angle = angle_0
+next_length = length_0
+next_point = point_0
+
+while next_length > 1:
+    next_point = branch(point=next_point, angle=next_angle, length=next_length)
+    next_angle = next_angle - 30
+    next_length = next_length * .75
 
 # сделать функцию branch рекурсивной
 
