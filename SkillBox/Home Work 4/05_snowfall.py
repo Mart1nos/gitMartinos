7a4479debd2fc7fd45c36c5cb1a0d06930ccc388
gitMartinos <stt7693@gmail.com>
2022-07-03 12:17:13 +0300

    fail lessen

--- SkillBox/Home Work 4/05_snowfall.py
+++ SkillBox/Home Work 4/05_snowfall.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import simple_draw as sd
-from simple_draw import Point
-
+from random import uniform
 sd.resolution = (1200, 600)
 
 # На основе кода из практической части реализовать снегопад:
@@ -20,21 +19,6 @@ N = 20
 # sd.user_want_exit()
 
 
-while True:
-    sd.clear_screen()
-    for x in range(50, 1050, 50):
-        y = 570
-        point = sd.get_point(x, y)
-        sd.snowflake(center=point, length=25)
-        y -= 10
-        if y < 50:
-            break
-
-    sd.sleep(0.1)
-    if sd.user_want_exit():
-        break
-
-sd.pause()
 
 # подсказка! для ускорения отрисовки можно
 #  - убрать clear_screen()
