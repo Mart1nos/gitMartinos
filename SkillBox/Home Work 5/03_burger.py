739c77c51e7939251b2e164c093b4681724f96f5
gitMartinos <stt7693@gmail.com>
2022-07-04 21:46:47 +0300

    complite 2 homeworks

--- SkillBox/Home Work 5/03_burger.py
+++ SkillBox/Home Work 5/03_burger.py
@@ -14,5 +14,18 @@
 
 # Создать рецепт своего бургера, по вашему вкусу.
 # Если не хватает инградиентов - создать соответствующие функции в модуле my_burger
+from my_burger.bread import bread
+from my_burger.cutlet import cutlet
+from my_burger.cheese import cheese
+from my_burger.tomato import tomato
+from my_burger.cucumber import cucumber
+from my_burger.mayonnaise import mayonnaise
 
-from
+print("Рецепт создания вкуснейшего бургера")
+bread()
+cutlet()
+cheese()
+tomato()
+cucumber()
+mayonnaise()
+
---
+++ SkillBox/Home Work 5/my_burger/double_cheeseburger.py
@@ -0,0 +1,22 @@
+from bread import bread
+from cutlet import cutlet
+from cheese import cheese
+from tomato import tomato
+from cucumber import cucumber
+from mayonnaise import mayonnaise
+from salad import salad
+from bacon import bacon
+
+print("Рецепт создания вкуснейшего двойного чизбургера")
+bread()
+salad()
+tomato()
+cutlet()
+cheese()
+cutlet()
+cheese()
+bacon()
+cucumber()
+mayonnaise()
+
+
