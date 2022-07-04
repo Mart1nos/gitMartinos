eeb78ef4bd77080870fc073697e1b6a0622704c3
gitMartinos <stt7693@gmail.com>
2022-07-04 21:05:35 +0300

    add files

--- SkillBox/Home Work 5/01_inhabitants.py
+++ SkillBox/Home Work 5/01_inhabitants.py
@@ -3,6 +3,12 @@
 # Вывести на консоль жителей комнат (модули room_1 и room_2)
 # Формат: В комнате room_1 живут: ...
 
-# TODO здесь ваш код
+from room_1 import folks as rm1
+from room_2 import folks
+
+
+print("В комнате room_1 живут: ", rm1)
+print("В комнате room_2 живут: ", folks)
+
 
 
--- SkillBox/Home Work 5/02_district.py
+++ SkillBox/Home Work 5/02_district.py
@@ -5,8 +5,16 @@
 # подсказка: для вывода элементов списка через запятую можно использовать функцию строки .join()
 # https://docs.python.org/3/library/stdtypes.html#str.join
 
-# TODO здесь ваш код
+from district.central_street.house1.room1 import folks as csh1r1
+from district.central_street.house1.room2 import folks as csh1r2
+from district.central_street.house2.room1 import folks as csh2r1
+from district.central_street.house2.room2 import folks as csh2r2
+from district.soviet_street.house1.room1 import folks as ssh1r1
+from district.soviet_street.house1.room2 import folks as ssh1r2
+from district.soviet_street.house2.room1 import folks as ssh2r1
+from district.soviet_street.house2.room2 import folks as ssh2r2
 
 
-
+print("На районе живут:", ",".join(csh1r1),",", ",".join(csh1r1),",", ",".join(csh1r2),",", ",".join(csh2r1), ",".join(csh2r2),",", ",".join(ssh1r1),
+      ",", ",".join(ssh1r2),",", ",".join(ssh2r1),",", ",".join(ssh2r2))
 
