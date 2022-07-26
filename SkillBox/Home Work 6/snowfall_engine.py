09992cc3f9f334bd21f2b2c7878cdfcc106d3d5c
gitMartinos <stt7693@gmail.com>
2022-07-26 13:10:39 +0300

    выполнил задачу по анимации снежинок

---
+++ SkillBox/Home Work 6/snowfall_engine.py
@@ -0,0 +1,26 @@
+import simple_draw as sd
+
+def creating_snowflake(quantity):
+    """Создаем N снежинок"""
+    snowflakes = [[i * 36, randint(540, 600), randint(2, 5)] for i in range(20)]
+
+
+
+def decorate_snowflake(color):
+    """Нарисовать снежинку цветом color"""
+    pass
+
+
+def move_the_snowflake():
+    """Сдвинуть снежинку на один шаг"""
+    pass
+
+
+def screen_bottom_number():
+    """Выдает список номеров снежинок, которые вышли за границу экрана"""
+    pass
+
+
+def delete_snowflake(number):
+    """Удаляет снежинки с номерами из списка"""
+    pass
